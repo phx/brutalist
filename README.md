f40edc2afd969ce59ee39a420cdae12bfe7b4128
phx <oldjamey@gmail.com>
2019-11-13 13:42:52 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+![Platform: ALL](https://img.shields.io/badge/platform-ALL-green)
+![Dependencies: python3+](https://img.shields.io/badge/dependencies-python3+-blue)
+![Version: 1.0](https://img.shields.io/badge/version-1.0-green)
+![Follow @rubynorails on Twitter](https://img.shields.io/twitter/follow/rubynorails?label=follow&style=social)
+
+# brutalist
+`brutalist` is a python3-based command-line tool for all platforms that can be used to generate quick and large word lists from 1 or more sample passwords that you provide.
+
+### Use case:
+Let's say you come across an outdated password in an old credential dump, but the user has since updated their password (`password`) to `P@$$w0rd123!`.  If fed the word `password`, `brutalist` will generate 773,604 unique custom permutations of the word, using various methods of leet speak substitution, as well as other common special character substitutions, suffixes, and special character additions -- all while keeping the order of the original characters in the password.
+
+## Install via Git:
+1. `git clone https://github.com/phx/brutalist.git`
+2. `cd brutalist`
+3. `sudo cp brutalist.py /usr/local/bin/`
+
+You can just as easily execute it where it stands or manually copy it elsewhere in your `$PATH`.
+
+## Install via Homebrew:
+1. `brew tap phx/brutalist`
+2. `brew install brutalist`
+
+## Run:
+```
+Usage: brutalist <[password] | -i [input file]>
+
+Options:
+
+       [no params]        takes input from stdin.
+       [string]           first argument is used as password.
+       -i [input file]    file is used as input.
+```
+## Examples
+- From the downloaded git repo working directory:
+ `./brutalist.py [a single password]`
+- Installed via `homebrew` or copied to your `$PATH` as `brutalist`:
+` brutalist [a single password]`
+ 
+Going forward, we will reference the command as `brutalist` -- just know that if it's not in your `$PATH`, it will have to be run in-place as `./brutalist.py`.
+
+- `stdin`from pipe, `stdout` to a file:
+`cat small_list.txt | brutalist > huge_list.txt`
+- `stdin` from input redirection, `stdout` to a file:
+`brutalist < small_list.txt > huge_list.txt`
+- input file as argument, write to both file and `stdout`:
+`brutalist -i /path/to/small_list.txt | tee huge_list.txt`
+- `stdin` from user input (outputs to `stdout`):
+`brutalist`[type your sample password(s), then hit `Ctrl-D`]
+
+
+### Background
+This program is by no means perfect.  It's quick and dirty, and by no means optimized.  I started out in C before realizing that Python would do a much better job, and it's still extremely fast when it comes to generating these word lists.  Almost instant.
+
+I wrote this over the course of a few hours because it was something I had been thinking about that solved a particular use-case I had come across on multiple occasions, and while BurpSuite Pro offers some similar functionality, you have to fool around with it every time to get something close to what you want, as opposed to just running a simple command using `brutalist` . 
+
+### To-Do, Updates, and Contribution
+I plan to add the option to specify the special characters used in the suffix, which can greatly cut down on the number of permutations for those who need shorter lists.  There will be some permutations that are missing, which I may add code for at a later point.  If you want to contribute or clean up the code, feel free to submit a pull request.
+
+### Disclaimer
+By downloading and running this software, you agree to only use it for ethical purposes and also agree to be held fully liable and accountable for any damage or harm caused by using `brutalist`.
---
+++ brutalist.py
@@ -0,0 +1,278 @@
+#!/usr/bin/python3
+##############################################################################
+#    Copyright (C) 2019  phx <https://github.com/phx>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##############################################################################
+
+import sys, re
+
+def show_help():
+    print()
+    print("Usage: brutalist <[password] | -i [input file]>\n")
+    print("Options:\n")
+    print("       [no params]        takes input from stdin.")
+    print("       [string]           first argument is used as password.")
+    print("       -i [input file]    file is used as input.\n")
+    sys.exit()
+
+def resubs(spass):
+    spass = spass.strip()
+    tmp = re.sub(r'[Aa]', '4', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Aa]', '@', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Bb]', '3', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Bb]', '13', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Cc]', '(', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Dd]', '[)', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Ee]', '3', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Ff]', '|=', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Gg]', '6', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Hh]', '|-|', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Ii]', '1', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Ii]', '|', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Jj]', '.]', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Kk]', '|<', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Ll]', '1', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Mm]', '|Y|', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Nn]', '/V', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Oo]', '0', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Pp]', '|>', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Qq]', '0,', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Rr]', '|2', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Ss]', '5', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Ss]', '$', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Tt]', '7', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Tt]', '+', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Uu]', '[_]', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Xx]', '}-{', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Yy]', '`/', spass)
+    subappend(tmp)
+    tmp = re.sub(r'[Zz]', '2', spass)
+    subappend(tmp)
+
+def subappend(tmp):
+    tmp = tmp.strip()
+    if tmp not in subs:
+        subs.append(tmp)
+        resubs(tmp)
+
+def get_subs(ch, char):
+    spass = before + char + after
+    spass = spass.strip()
+    subs.append(re.sub(r'[Aa]', '4', spass))
+    subs.append(re.sub(r'[Aa]', '@', spass))
+    subs.append(re.sub(r'[Bb]', '3', spass))
+    subs.append(re.sub(r'[Bb]', '13', spass))
+    subs.append(re.sub(r'[Cc]', '(', spass))
+    subs.append(re.sub(r'[Dd]', '[)', spass))
+    subs.append(re.sub(r'[Ee]', '3', spass))
+    subs.append(re.sub(r'[Ff]', '|=', spass))
+    subs.append(re.sub(r'[Gg]', '6', spass))
+    subs.append(re.sub(r'[Hh]', '|-|', spass))
+    subs.append(re.sub(r'[Ii]', '1', spass))
+    subs.append(re.sub(r'[Ii]', '|', spass))
+    subs.append(re.sub(r'[Jj]', '.]', spass))
+    subs.append(re.sub(r'[Kk]', '|<', spass))
+    subs.append(re.sub(r'[Ll]', '1', spass))
+    subs.append(re.sub(r'[Mm]', '|Y|', spass))
+    subs.append(re.sub(r'[Nn]', '/V', spass))
+    subs.append(re.sub(r'[Oo]', '0', spass))
+    subs.append(re.sub(r'[Pp]', '|>', spass))
+    subs.append(re.sub(r'[Qq]', '0,', spass))
+    subs.append(re.sub(r'[Rr]', '|2', spass))
+    subs.append(re.sub(r'[Ss]', '5', spass))
+    subs.append(re.sub(r'[Ss]', '$', spass))
+    subs.append(re.sub(r'[Tt]', '7', spass))
+    subs.append(re.sub(r'[Tt]', '+', spass))
+    subs.append(re.sub(r'[Uu]', '[_]', spass))
+    subs.append(re.sub(r'[Xx]', '}-{', spass))
+    subs.append(re.sub(r'[Yy]', '`/', spass))
+    subs.append(re.sub(r'[Zz]', '2', spass))
+    c = char.lower()
+    for sc in range(len(spass)):
+        b = spass[:sc]
+        a = spass[sc+1:]
+        if ch == sc:
+            if c == 'a':
+                subs.append(b + '4' + a)
+                subs.append(b + '@' + a)
+            if c == 'b':
+                subs.append(b + '3' + a)
+                subs.append(b + '13' + a)
+            if c == 'c':
+                subs.append(b + '(' + a)
+            if c == 'd':
+                subs.append(b + '[)' + a)
+            if c == 'e':
+                subs.append(b + '3' + a)
+            if c == 'f':
+                subs.append(b + '|=' + a)
+            if c == 'g':
+                subs.append(b + '6' + a)
+            if c == 'h':
+                subs.append(b + '|-|' + a)
+            if c == 'i':
+                subs.append(b + '|' + a)
+                subs.append(b + '1' + a)
+            if c == 'l':
+                subs.append(b + 'l' + a)
+            if c == 'm':
+                subs.append(b + '|Y|' + a)
+            if c == 'n':
+                subs.append(b + '/V' + a)
+            if c == 'o':
+                subs.append(b + '0' + a)
+            if c == 'p':
+                subs.append(b + '|>' + a)
+            if c == 'q':
+                subs.append(b + '0,' + a)
+            if c == 'r':
+                subs.append(b + '|2' + a)
+            if c == 's':
+                subs.append(b + '5' + a)
+                subs.append(b + '$' + a)
+            if c == 't':
+                subs.append(b + '7' + a)
+                subs.append(b + '+' + a)
+            if c == 'u':
+                subs.append(b + '[_]' + a)
+            if c == 'x':
+                subs.append(b + '}-{' + a)
+            if c == 'y':
+                subs.append(b + '`/' + a)
+            if c == 'z':
+                subs.append(b + '2' + a)
+
+
+if __name__ == '__main__':
+
+    password_list = []
+
+    if 'help' in sys.argv:
+        show_help()
+    if '-help' in sys.argv:
+        show_help()
+    if '--help' in sys.argv:
+        show_help()
+    if '-h' in sys.argv:
+        show_help()
+    if len(sys.argv) == 1:
+        try:
+            passwords = [x for x in sys.stdin.readlines()]
+            for password in passwords:
+                password_list.append(password.strip())
+        except:
+            show_help
+    elif len(sys.argv) == 2:
+        try:
+            password_list.append(sys.argv[1].strip())
+            # password = sys.argv[1].strip()
+        except:
+            show_help
+    elif len(sys.argv) == 3:
+        try:
+            password_file = sys.argv[3]
+            with open(password_file, r) as fp:
+                for line in fp:
+                    password_list.append(line.strip())
+        except:
+            show_help
+    
+    passwords = set(password_list)
+
+    subs = []
+
+    for password in passwords:    
+        resubs(password)
+        
+        # switch to upper
+        up = password.upper()
+        resubs(up)
+        for ch in range(len(up)):
+            before = up[:ch]
+            char = up[ch]
+            after = up[ch+1:]
+            uchar = up[ch].replace(char, char.lower())
+            subs.append(before + uchar + after)
+            resubs(before + uchar + after)
+            get_subs(ch, uchar)
+
+        # switch to lower
+        lp = password.lower()
+        resubs(lp)
+        for ch in range(len(lp)):
+            before = lp[:ch]
+            char = lp[ch]
+            after = lp[ch+1:]
+            lchar = lp[ch].replace(char, char.upper())
+            subs.append(before + lchar + after)
+            resubs(before + lchar + after)
+            get_subs(ch, lchar)
+
+        unique_subs = set(subs)
+
+        number_suffixes = []
+        numbers = '0123456789'
+        special_characters = '!@#$%^&*+-_.;~()[]'
+
+        for ch in range(len(numbers)):
+            if 9 > ch >= 0:    
+                number_suffixes.append(numbers[ch-1:ch+2])
+        number_suffixes.remove('')
+
+        for sub in unique_subs:
+            print(sub)
+        for sub in unique_subs:
+            for num in numbers:
+                print(sub + num)
+                print(sub + num + num)
+        for sub in unique_subs:
+            for suf in number_suffixes:
+                print(sub + suf)
+                for c in special_characters:
+                    print(sub + suf + c)
+        for sub in unique_subs:
+            for c in special_characters:
+                print(sub + c)
+                for num in numbers:
+                    print(sub + num + c)
+                    print(sub + num + num + c)
+
